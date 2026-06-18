e705aff86bcc231edb02165e4ecfea78b51d1cb5
Tong Thien Viet Anh <fei.yang7641@gmail.com>
2026-06-18 09:54:29 +0700

    chore: fetch matches by FIFA seasonId to simplify query params

--- next.config.js
+++ next.config.js
@@ -18,6 +18,11 @@ const config = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "api.fifa.com",
+        pathname: "/api/v3/picture/**",
+      },
+      {
+        protocol: "https",
         hostname: "crests.football-data.org",
       },
       {
